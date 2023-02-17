8204a4b2ae444c7d31c18cce882ef8f50f5ae80e
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-02-17 03:49:42 -0500

    Initial Commit

---
+++ ListOfPhotoMetadate/ListOfPhotoMetadateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfPhotoMetadateApp.swift
+//  ListOfPhotoMetadate
+//
+//  Created by Sergey Lukaschuk on 2023-02-17.
+//
+
+import SwiftUI
+
+@main
+struct ListOfPhotoMetadateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
